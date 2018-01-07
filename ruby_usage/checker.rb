cb5cdc62eefddbf67053ab3412a905d0b6064478
luan devecchi <sillonfox@live.com>
2018-01-07 17:37:32 -0200

    Ruby usage for php api

---
+++ ruby_usage/checker.rb
@@ -0,0 +1,22 @@
+require 'uri'
+require 'net/http'
+require 'colorize'
+
+file='mylist.txt'
+File.readlines(file).each do |line|
+  splitfields = line.split(":")
+  senhafix = splitfields[1].gsub("\n" , "")
+  tocheck = "http://localhost/chk/api/?u=#{splitfields[0]}&p=#{senhafix}"
+  result = Net::HTTP.get(URI.parse(tocheck))
+	
+  if result.include? "PREMIUM"
+  	puts"[LIVE] #{splitfields[0]} #{senhafix} | PREMIUM".green
+  elsif result.include? "FREE"
+   	puts"[LIVE] #{splitfields[0]} #{senhafix} | FREE".yellow
+  else
+  	puts"[DIE] #{splitfields[0]} #{senhafix}".red
+
+  end
+end
+
+puts'finished'
